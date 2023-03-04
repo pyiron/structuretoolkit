c7095eb13524ee515ccd29affd057e2e0ea54ad2
Jan Janssen <janssen@lanl.gov>
2023-03-03 18:42:42 -0700

    add strain test

--- structuretoolkit/__init__.py
+++ structuretoolkit/__init__.py
@@ -25,8 +25,9 @@ from structuretoolkit.analyse.symmetry import get_symmetry
 
 # Build
 from structuretoolkit.build.aimsgb import grainboundary_build, grainboundary_info
-from structuretoolkit.build.sqs import get_sqs_structures
 from structuretoolkit.build.compound import B2, C14, C15, C36, D03
+from structuretoolkit.build.sqs import get_sqs_structures
+from structuretoolkit.build.surface import high_index_surface, high_index_surface_info
 
 # Other
 from structuretoolkit.helper import (
