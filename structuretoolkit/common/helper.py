59b0fe026e913517097c0013b7620ceb79663019
Sam Dareska <37879103+samwaseda@users.noreply.github.com>
2023-05-25 16:46:33 +0200

    use ASE-native get_atomic_numbers

--- structuretoolkit/common/helper.py
+++ structuretoolkit/common/helper.py
@@ -4,7 +4,7 @@ from scipy.sparse import coo_matrix
 
 
 def get_atomic_numbers(structure):
-    return [atomic_numbers[el] for el in structure.get_chemical_symbols(parent=True)]
+    return structure.get_atomic_numbers()
 
 
 def get_extended_positions(
