af6decd3add2ef77dbefc3f3c66136a84891dd98
Marvin Poul <poul@mpie.de>
2023-07-19 16:15:36 +0200

    Add pyxtal wrapper
    Co-authored-by: Jan Janssen <jan-janssen@users.noreply.github.com>

---
+++ tests/test_pyxtal.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from ase import Atoms
+from structuretoolkit.build.pyxtal import pyxtal
+
+class TestPyxtal(TestCase):
+
+    def test_args_raised(self):
+        """pyxtal should raise appropriate errors when called with wrong arguments"""
+
+        with self.assertRaises(ValueError, msg="No error raised when num_ions and species do not match!"):
+            pyxtal(1, species=['Fe'], num_ions=[1,2])
+
+        with self.assertRaises(ValueError, msg="No error raised when num_ions and species do not match!"):
+            pyxtal(1, species=['Fe', 'Cr'], num_ions=[1])
+
+        try:
+            pyxtal([193, 194], ['Mg'], num_ions=[1], allow_exceptions=True)
+        except ValueError:
+            self.fail("Error raised even though allow_exceptions=True was passed!")
+
+        with self.assertRaises(ValueError, msg="No error raised even though allow_exceptions=False was passed!"):
+            pyxtal(194, ['Mg'], num_ions=[1], allow_exceptions=False)
+
+    def test_return_value(self):
+        """pyxtal should either return Atoms or list of dict, depending on arguments"""
+
+        self.assertIsInstance(pyxtal(1, species=['Fe'], num_ions=[1]), Atoms,
+                              "returned not an Atoms with scalar arguments")
+        self.assertIsInstance(pyxtal([1,2], species=['Fe'], num_ions=[1]), list,
+                              "returned not a StructureStorage with multiple groups")
+        self.assertIsInstance(pyxtal(1, species=['Fe'], num_ions=[1], repeat=5), list,
+                              "returned not a StructureStorage with repeat given")
+        self.assertEqual(len(pyxtal(1, species=['Fe'], num_ions=[1], repeat=5)), 5,
+                         "returned number of structures did not match given repeat")
+        self.assertTrue(all(isinstance(d, dict) for d in pyxtal(1, species=['Fe'], num_ions=[1], repeat=5)),
+                        "returned list should contain only dicts")
