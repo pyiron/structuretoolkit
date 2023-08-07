2333913b277e7d488a14183b5fd504f31da79603
samwaseda <o.waseda@mpie.de>
2023-08-07 15:07:22 +0000

    satisfy Black

--- structuretoolkit/visualize.py
+++ structuretoolkit/visualize.py
@@ -205,7 +205,7 @@ def _plot3d_plotly(
     )
     fig.update_layout(scene_camera=angle)
     fig.update_traces(marker=dict(line=dict(width=0.1, color="DarkSlateGrey")))
-    fig.update_scenes(aspectmode='data')
+    fig.update_scenes(aspectmode="data")
     return fig
 
 
