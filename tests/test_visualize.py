357303b3ff240e8a452c940dbcfc5ce968c937ca
samwaseda <o.waseda@mpie.de>
2024-01-12 09:46:11 +0000

    add tests for get_frame

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -4,7 +4,7 @@
 
 import unittest
 import numpy as np
-from structuretoolkit.visualize import _get_flattened_orientation
+from structuretoolkit.visualize import _get_flattened_orientation, _get_frame
 
 
 class TestAtoms(unittest.TestCase):
@@ -20,6 +20,19 @@ class TestAtoms(unittest.TestCase):
         R = np.random.random(9).reshape(-1, 3)
         R = np.array(_get_flattened_orientation(R, 1)).reshape(4, 4)
         self.assertAlmostEqual(np.linalg.det(R), 1)
+
+    def test_get_frame(self):
+        frame = _get_frame(np.eye(3))
+        self.assertLessEqual(
+            np.unique(frame.reshape(-1, 6), axis=0, return_counts=True)[1].max(),
+            1
+        )
+        dx, counts = np.unique(
+            np.diff(frame, axis=-2).squeeze().astype(int), axis=0, return_counts=True
+        )
+        self.assertEqual(dx.ptp(), 1)
+        self.assertEqual(counts.min(), 4)
+        self.assertEqual(counts.max(), 4)
 
 
 if __name__ == "__main__":
