708a64304455b09e909231014413a151d4b144b0
Marvin Poul <poul@mpie.de>
2024-02-14 18:01:32 +0100

    Recreate structure fresh during get_primitive_cell
    This avoids a problem, when you pass standardize=True and the resulting cell would be larger than the original.

--- structuretoolkit/analyse/symmetry.py
+++ structuretoolkit/analyse/symmetry.py
@@ -345,26 +345,35 @@ class Symmetry(dict):
         >>> len(symmetry.get_primitive_cell()) == len(basis)
         True
         """
+        if not all(self._structure.pbc):
+            raise ValueError("Can only symmetrize periodic structures.")
         ret = spglib.standardize_cell(
             self._get_spglib_cell(use_elements=use_elements, use_magmoms=use_magmoms),
             to_primitive=not standardize,
         )
         if ret is None:
             raise SymmetryError(spglib.spglib.spglib_error.message)
-        cell, positions, indices = ret
-        positions = (cell.T @ positions.T).T
-        new_structure = self._structure.copy()
-        new_structure.cell = cell
-        new_structure = new_structure[: len(indices)]
+        cell, scaled_positions, indices = ret
         indices_dict = {
             v: k
             for k, v in structuretoolkit.common.helper.get_species_indices_dict(
                 structure=self._structure
             ).items()
         }
-        new_structure.symbols = [indices_dict[i] for i in indices]
-        new_structure.positions = positions
+        symbols = [indices_dict[i] for i in indices]
+        arrays = {k: self._structure.arrays[k] for k in self._structure.arrays
+                        if k not in ('numbers', 'positions')}
+        new_structure = type(self._structure)(
+            symbols=symbols,
+            scaled_positions=scaled_positions,
+            cell=cell,
+            pbc=[True, True, True]
+        )
+        for k, a in arrays.items():
+            new_structure.arrays[k] = a
+
         return new_structure
+
 
     def get_ir_reciprocal_mesh(
         self,
