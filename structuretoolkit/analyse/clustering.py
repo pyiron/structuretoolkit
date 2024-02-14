e90d7306ccbf21834690bfb5ef8276161e493456
pyiron-runner <pyiron@mpie.de>
2024-02-14 13:24:21 +0000

    Format black

--- structuretoolkit/analyse/clustering.py
+++ structuretoolkit/analyse/clustering.py
@@ -5,10 +5,11 @@ from ase.atoms import Atoms
 from ase.visualize import view
 import matplotlib.pyplot as plt
 
+
 def compute_cluster_labels(structure, num_clusters):
     """
     Compute hierarchical clustering labels for an ASE Atoms structure.
-    
+
     Use case: Identification of inherently different parts of a single structure, i.e. separate slabs, specific phases, etc.
     Atomic distances are the sole defining metric used for clustering.
 
@@ -23,18 +24,21 @@ def compute_cluster_labels(structure, num_clusters):
         # If structure is an ASE Atoms object, use it directly
         struct = structure
     else:
-        raise ValueError("Invalid input for structure. Please provide an ASE Atoms object.")
+        raise ValueError(
+            "Invalid input for structure. Please provide an ASE Atoms object."
+        )
 
     # Calculate the distance matrix
     distance_matrix = struct.get_all_distances(mic=True)
 
     # Perform hierarchical clustering
-    linkage_matrix = linkage(distance_matrix, method='ward')
+    linkage_matrix = linkage(distance_matrix, method="ward")
 
     # Get cluster labels for the specified number of clusters
-    cluster_labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
+    cluster_labels = fcluster(linkage_matrix, num_clusters, criterion="maxclust")
 
     return cluster_labels
+
 
 def ase_view_clusters(structure, n_clusters, target_cluster_label=1):
     """
@@ -49,7 +53,8 @@ def ase_view_clusters(structure, n_clusters, target_cluster_label=1):
     indices_of_cluster = np.where(cluster_labels == target_cluster_label)[0]
     # Visualize the cluster using ASE's view function
     view(structure[indices_of_cluster])
-    
+
+
 def plot_clusters(structure, n_clusters=1, projection=[1, 2], figsize=(30, 10)):
     """
     Plot clusters in a 2D scatter plot based on hierarchical clustering.
@@ -70,18 +75,25 @@ def plot_clusters(structure, n_clusters=1, projection=[1, 2], figsize=(30, 10)):
 
     for cluster_label in np.unique(cluster_labels):
         cluster_data = structure.positions[cluster_labels == cluster_label]
-        ax.scatter(cluster_data[:, projection[0]], cluster_data[:, projection[1]], label=f'Cluster {cluster_label}')
+        ax.scatter(
+            cluster_data[:, projection[0]],
+            cluster_data[:, projection[1]],
+            label=f"Cluster {cluster_label}",
+        )
 
-    ax.set_xlabel(f'Axis {projection[0]}')
-    ax.set_ylabel(f'Axis {projection[1]}')
-    ax.set_title(f'2D Projection (Axis {projection[0]}-{projection[1]}) \nHierarchical Clusters ')
-    ax.legend(loc=[1.05,0.9])
+    ax.set_xlabel(f"Axis {projection[0]}")
+    ax.set_ylabel(f"Axis {projection[1]}")
+    ax.set_title(
+        f"2D Projection (Axis {projection[0]}-{projection[1]}) \nHierarchical Clusters "
+    )
+    ax.legend(loc=[1.05, 0.9])
 
     # Set aspect ratio to be equal
-    ax.set_aspect('equal')
+    ax.set_aspect("equal")
 
     # Set axis limits to be tight
     ax.autoscale()
+
 
 def get_structure_clusters(structure, n_clusters=2):
     """
@@ -94,7 +106,7 @@ def get_structure_clusters(structure, n_clusters=2):
     Returns:
         list: List of ASE Atoms structures, each corresponding to a cluster.
     """
-    # Returns a list of structures 
+    # Returns a list of structures
     cluster_labels = compute_cluster_labels(structure, n_clusters)
     struct_list = []
     for target_cluster_label in np.unique(cluster_labels):
