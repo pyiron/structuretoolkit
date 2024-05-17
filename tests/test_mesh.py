d0cff80310a94ffca469f23f11451635f94ee23a
samwaseda <o.waseda@mpie.de>
2024-05-17 15:56:04 +0000

    create tests

---
+++ tests/test_mesh.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import unittest
+import numpy as np
+from ase.build import bulk
+import structuretoolkit as stk
+
+
+
+class TestMesh(unittest.TestCase):
+    def test_mesh(self):
+        structure = bulk("Al", cubic=True)
+        self.assertEqual(stk.create_mesh(structure, n_mesh=4).shape, (4, 4, 4, 3))
+        with self.assertRaises(ValueError):
+            stk.create_mesh(structure, n_mesh=None, density=None)
+        self.assertEqual(stk.create_mesh(structure, n_mesh=10, density=structure.cell[0, 0] / 4).shape, (10, 10, 10, 3))
+        self.assertEqual(stk.create_mesh(structure, n_mesh=None, density=structure.cell[0, 0] / 4).shape, (4, 4, 4, 3))
+
+
+if __name__ == "__main__":
+    unittest.main()
