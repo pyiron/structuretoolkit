272abe282f911c4f95cabd10fe1cd62c1b3a7436
Jan Janßen <janssen@mpie.de>
2024-07-17 20:57:27 +0200

    Skip spglib tests when spglib is not available

--- tests/test_analyse_phonopy.py
+++ tests/test_analyse_phonopy.py
@@ -3,6 +3,17 @@ from ase.build import bulk
 from structuretoolkit.analyse.phonopy import get_equivalent_atoms
 
 
+try:
+    import spglib
+
+    spglib_not_available = False
+except ImportError:
+    spglib_not_available = True
+
+
+@unittest.skipIf(
+    spglib_not_available, "spglib is not installed, so the spglib tests are skipped."
+)
 class TestEquivalentAtoms(unittest.TestCase):
     def test_get_equivalent_atoms(self):
         equivalent_atoms = get_equivalent_atoms(
