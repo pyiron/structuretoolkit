a68d0b15191809b05a9b5bd643026c87ca663ba1
Marvin Poul <ponder@creshal.de>
2025-10-14 12:10:16 +0200

    Remove pyxtal wrapper
    This lives now directly in the assyst package >0.8.1

--- structuretoolkit/build/__init__.py
+++ structuretoolkit/build/__init__.py
@@ -1,7 +1,6 @@
 from structuretoolkit.build.aimsgb import get_grainboundary_info, grainboundary
 from structuretoolkit.build.compound import B2, C14, C15, C36, D03
 from structuretoolkit.build.mesh import create_mesh
-from structuretoolkit.build.random import pyxtal
 from structuretoolkit.build.sqs import sqs_structures
 from structuretoolkit.build.surface import (
     get_high_index_surface_info,
@@ -17,7 +16,6 @@ __all__ = [
     "C36",
     "D03",
     "create_mesh",
-    "pyxtal",
     "sqs_structures",
     "get_high_index_surface_info",
     "high_index_surface",
