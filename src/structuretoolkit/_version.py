b8a6f41c814d936452b3fd5e5b3e1e4f4085506f
pyiron-runner <pyiron@mpie.de>
2026-04-03 15:54:47 +0000

    Format black

--- src/structuretoolkit/_version.py
+++ src/structuretoolkit/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev1+gd17555710.d20260403'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'gd17555710.d20260403')
+__version__ = version = "0.1.dev1+gd17555710.d20260403"
+__version_tuple__ = version_tuple = (0, 1, "dev1", "gd17555710.d20260403")
 
 __commit_id__ = commit_id = None
