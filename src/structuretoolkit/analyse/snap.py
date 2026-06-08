74f1cfa412c250d9d93ec3468928f399907b8a1a
Jan Janssen <janssen@mpie.de>
2026-06-08 11:57:31 +0200

    Use extract_atom() rather than extract_atom_iarray()

--- src/structuretoolkit/analyse/snap.py
+++ src/structuretoolkit/analyse/snap.py
@@ -547,12 +547,12 @@ def _extract_computes_snap(
     Returns:
         np.ndarray: Output of the LAMMPS compute command
     """
-    lmp_atom_ids = lmp.numpy.extract_atom_iarray("id", num_atoms).flatten()
+    lmp_atom_ids = lmp.numpy.extract_atom(name="id").flatten()
     cond = bool(np.all(lmp_atom_ids == 1 + np.arange(num_atoms)))
     assert cond, "LAMMPS seems to have lost atoms"
 
     # Extract types
-    lmp_types = lmp.numpy.extract_atom_iarray(name="type", nelem=num_atoms).flatten()
+    lmp_types = lmp.numpy.extract_atom(name="type").flatten()
     lmp_volume = lmp.get_thermo("vol")
 
     # Extract Bsum
